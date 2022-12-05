6e40370a98d1bc420ff604eb8a1a3af9e9b8681a
Muhsin TT <muhsintt777@gmail.com>
2022-12-06 05:09:02 +0530

    added form and controlled input

--- src/components/content/feed/postForm/PostForm.jsx
+++ src/components/content/feed/postForm/PostForm.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PostForm.css";
+import { Avatar } from "@mui/material";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 
 const PostForm = () => {
-  return <div className="postForm-container">PostForm</div>;
+  const [textInput, setTextInput] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(textInput);
+  };
+
+  return (
+    <div className="postForm-container">
+      <form onSubmit={onSubmit}>
+        <div className="postForm-topSection">
+          <Avatar />
+          <div className="postForm-Form__inputText">
+            <input
+              value={textInput}
+              onChange={(e) => setTextInput(e.target.value)}
+              placeholder="Share something..."
+              type="text"
+              required
+            />
+            <button type="submit">
+              <CreateOutlinedIcon />
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PostForm;
