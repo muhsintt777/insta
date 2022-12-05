fcd7395f06f33d530115f753b91729600f3dbef6
Muhsin TT <muhsintt777@gmail.com>
2022-12-06 01:52:41 +0530

    added postform

--- src/components/content/feed/Feed.jsx
+++ src/components/content/feed/Feed.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Feed.css";
+import PostForm from "./postForm/PostForm";
 
 const Feed = () => {
-  return <div className="feed-container">Feed</div>;
+  return (
+    <div className="feed-container">
+      <PostForm />
+    </div>
+  );
 };
 
 export default Feed;
---
+++ src/components/content/feed/postForm/PostForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PostForm.css";
+
+const PostForm = () => {
+  return <div className="postForm-container">PostForm</div>;
+};
+
+export default PostForm;
