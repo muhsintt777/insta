237d9dbb103fce7dba14b2ebbcc36e5ee2ce1350
Muhsin TT <muhsintt777@gmail.com>
2022-12-06 21:53:52 +0530

    added postcard description imagesection

--- src/components/content/feed/postCard/PostCard.jsx
+++ src/components/content/feed/postCard/PostCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PostCard.css";
+import danielPic from "../../../../images/daniel.jpg";
 import { Avatar } from "@mui/material";
 import { OpenInNewTwoTone } from "@mui/icons-material";
 
@@ -17,6 +18,15 @@ const PostCard = () => {
         <div className="postCard-topSection__optionButton">
           <OpenInNewTwoTone />
         </div>
+      </div>
+      <div className="postCard-description">
+        <p className="postCard-description__messagePara">
+          Was great meeting up with Anna and Dave Bishop at the breakfast talk!
+        </p>
+        <p className="postCard-description__hashtagPara">#breakfast</p>
+      </div>
+      <div className="postCard-imageSection">
+        <img className="postCard-imageSection__img" src={danielPic} alt="img" />
       </div>
     </article>
   );
