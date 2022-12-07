2e1d22dd1ccff24696835cf7d6f69d09e3e6c00e
Muhsin TT <muhsintt777@gmail.com>
2022-12-08 03:43:08 +0530

    updated postcard buttons css

--- src/components/content/feed/postCard/PostCard.jsx
+++ src/components/content/feed/postCard/PostCard.jsx
@@ -8,6 +8,7 @@ import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import { Avatar } from "@mui/material";
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "../../../../firebase/config";
+import { grey } from "@mui/material/colors";
 
 const PostCard = ({ message, hashtags, id }) => {
   const handleDeletePost = async () => {
@@ -29,11 +30,13 @@ const PostCard = ({ message, hashtags, id }) => {
           <p className="postCard-topSection__name__namePara">Muhsin TT</p>
           <p className="postCard-topSection__name__timePara">2 hours ago</p>
         </div>
-        <div className="postCard-topSection__optionButton">
-          <button type="button" onClick={handleDeletePost}>
-            <MoreHorizOutlinedIcon />
-          </button>
-        </div>
+        <button
+          className="postCard-topSection__optionButton"
+          type="button"
+          onClick={handleDeletePost}
+        >
+          <MoreHorizOutlinedIcon sx={{ color: grey[700] }} />
+        </button>
       </div>
       <div className="postCard-description">
         <p className="postCard-description__messagePara">{message}</p>
@@ -44,16 +47,16 @@ const PostCard = ({ message, hashtags, id }) => {
       </div> */}
       <div className="postCard-bottomSection">
         <div className="postCard-bottomSection__reactionButtons">
-          <button>
-            <FavoriteBorderOutlinedIcon />
+          <button className="postCard-bottomSection__reactionButtons__btn">
+            <FavoriteBorderOutlinedIcon sx={{ color: grey[700] }} />
           </button>
-          <button>
-            <ChatBubbleOutlineRoundedIcon />
+          <button className="postCard-bottomSection__reactionButtons__btn">
+            <ChatBubbleOutlineRoundedIcon sx={{ color: grey[700] }} />
           </button>
         </div>
-        <div className="postCard-bottomSection__sendButton">
-          <SendOutlinedIcon />
-        </div>
+        <button type="button" className="postCard-bottomSection__sendButton">
+          <SendOutlinedIcon sx={{ color: grey[700] }} />
+        </button>
       </div>
     </article>
   );
