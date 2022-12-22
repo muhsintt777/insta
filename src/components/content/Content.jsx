28407e37e2307d9891b056f09d85c3fca65d8c98
Muhsin TT <muhsintt777@gmail.com>
2022-12-22 15:17:16 +0530

    moved bottomNav from content to layout

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,6 +1,5 @@
 import { grey } from "@mui/material/colors";
 import React from "react";
-import BottomNav from "../bottomNav/BottomNav";
 import "./Content.css";
 import Feed from "./feed/Feed";
 
@@ -8,7 +7,6 @@ const Content = () => {
   return (
     <div style={{ background: grey[100] }} className="content-container">
       <Feed />
-      <BottomNav />
     </div>
   );
 };
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import BottomNav from "../bottomNav/BottomNav";
 import Header from "../header/Header";
 
 const Layout = () => {
@@ -7,6 +8,7 @@ const Layout = () => {
     <>
       <Header />
       <Outlet />
+      <BottomNav />
     </>
   );
 };
