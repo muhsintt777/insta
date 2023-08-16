43d6786c557c30e5d36fce968b852107e3b24b17
muhsin <muhsintt777@gmail.com>
2023-08-17 01:28:38 +0530

    fix : import updateee

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@ import { collection, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAdk296Lue6O_q_etBHK60XIuQt8BoGcX4",
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
   authDomain: "insta-7ffa0.firebaseapp.com",
   projectId: "insta-7ffa0",
   storageBucket: "insta-7ffa0.appspot.com",
-  messagingSenderId: "385191573777",
-  appId: "1:385191573777:web:63329e123cb57c55cfb4b0",
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIRBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
