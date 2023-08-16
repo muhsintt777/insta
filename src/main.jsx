135dbf2c89b664f2f0f7dbf4773339ef9b1dc621
muhsin <muhsintt777@gmail.com>
2023-08-17 01:00:39 +0530

    converted to vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import store from "./features/store.js";
 import { persistStore } from "redux-persist";
-import store from "./app/store";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 let persistor = persistStore(store);
-root.render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
