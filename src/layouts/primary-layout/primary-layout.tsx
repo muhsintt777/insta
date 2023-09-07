883ae5ceb844c9a319a7b942be795bb210c378d4
Muhsin TT <muhsintt777@gmail.com>
2023-09-07 18:54:24 +0530

    feat : add header component

--- src/layouts/primary-layout/primary-layout.tsx
+++ src/layouts/primary-layout/primary-layout.tsx
@@ -1,9 +1,11 @@
 import styles from "./style.module.css";
 import { Outlet } from "react-router-dom";
+import { Header } from "components/header/header";
 
 export const PrimaryLayout = () => {
   return (
     <div className={styles.container}>
+      <Header />
       <p>primary layout</p>
       <Outlet />
     </div>
