78ee79094fcf5d296c661f1770eb078a2bb0f0f3
muhsin <muhsintt777@gmail.com>
2023-09-17 05:25:40 +0530

    feat : add validation

--- src/screens/auth/login/login.tsx
+++ src/screens/auth/login/login.tsx
@@ -3,31 +3,49 @@ import { ChangeEvent, useState } from "react";
 import { InputField } from "components/input-field/input-field";
 import { PrimaryButton } from "components/primary-button/primary-button";
 import { TitleHead } from "./title-head";
+import { REGEX } from "constants/validation";
+import { trimAllWhitespace } from "utils/common";
 
 interface EmailInpType {
   value: string;
   isValid: boolean;
+  error: null | string;
 }
 interface PasswordInpType {
   value: string;
   isValid: boolean;
+  error: null | string;
 }
 
 export const Login = () => {
   const [emailInp, setEmailInp] = useState<EmailInpType>({
     value: "",
     isValid: false,
+    error: null,
   });
   const [passwordInp, setPasswordInp] = useState<PasswordInpType>({
     value: "",
     isValid: false,
+    error: null,
   });
 
   function handleEmailInpChange(e: ChangeEvent<HTMLInputElement>) {
-    setEmailInp({ value: e.target.value, isValid: true });
+    const value = e.target.value;
+    const trimmedValue = trimAllWhitespace(value);
+    const isValid = REGEX.email.test(trimmedValue);
+    const error = !isValid && trimmedValue ? "Please enter valid email" : null;
+
+    setEmailInp({ value, isValid, error });
   }
+
   function handlePasswordInpChange(e: ChangeEvent<HTMLInputElement>) {
-    setPasswordInp({ value: e.target.value, isValid: true });
+    const value = e.target.value;
+    const trimmedValue = trimAllWhitespace(value);
+    const isValid = REGEX.password.test(trimmedValue);
+    const error =
+      !isValid && trimmedValue ? "Please enter valid password" : null;
+
+    setPasswordInp({ value, isValid, error });
   }
   return (
     <div className={styles.container}>
@@ -44,10 +62,10 @@ export const Login = () => {
               onChange={handleEmailInpChange}
               placeholder="john@email.com"
               type="text"
-              error={"testingg..."}
+              error={emailInp.error}
             />
             <InputField
-              error={null}
+              error={passwordInp.error}
               label="PASSWORD"
               name="password"
               onChange={handlePasswordInpChange}
