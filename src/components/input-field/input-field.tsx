aea4c9d6cd6c05bdc7306cb4b921b1d10c266cd2
muhsin <muhsintt777@gmail.com>
2023-09-17 03:44:53 +0530

    chore : create input field comp...

---
+++ src/components/input-field/input-field.tsx
@@ -0,0 +1,37 @@
+import { FC, ChangeEvent } from "react";
+import styles from "./input-fieldStyle.module.scss";
+
+interface InputFieldProps {
+  placeholder: string;
+  label: string;
+  type: "text" | "number";
+  name: string;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  error: string | null;
+}
+export const InputField: FC<InputFieldProps> = ({
+  placeholder,
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  error,
+}) => {
+  return (
+    <div className={styles.container}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <p className={error ? styles.errorMessageShow : styles.errorMessageHide}>
+        {error}
+      </p>
+    </div>
+  );
+};
