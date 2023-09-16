4f920cae71ad9f98c7596fa2ce315b967dcd6039
muhsin <muhsintt777@gmail.com>
2023-09-16 13:25:09 +0530

    feat : add auth route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PrimaryLayout } from "./layouts/primary-layout/primary-layout";
+import { AuthLayout } from "layouts/auth-layout/auth-layout";
 import { Home } from "./screens/home/home";
+import { Login } from "screens/home/auth/login/login";
 
 export const App = () => {
   return (
@@ -9,6 +11,9 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<PrimaryLayout />}>
           <Route index element={<Home />} />
+        </Route>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
