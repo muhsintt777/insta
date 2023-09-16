e35d1547e5aaf9ed6da163db3734e2eb56c3cc36
muhsin <muhsintt777@gmail.com>
2023-09-16 21:18:39 +0530

    feat : add login title

--- src/screens/auth/login/login.tsx
+++ src/screens/auth/login/login.tsx
@@ -1,3 +1,12 @@
+import styles from "./loginStyle.module.scss";
+import { TitleHead } from "./title-head";
+
 export const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.head}>
+        <TitleHead title="LOGIN" />
+      </div>
+    </div>
+  );
 };
