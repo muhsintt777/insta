80d2d3bf4da65f3f69b61c9692723765125a5844
muhsin <muhsintt777@gmail.com>
2023-09-17 04:28:50 +0530

    feat : add passwordinp, ui update

--- src/screens/auth/login/login.tsx
+++ src/screens/auth/login/login.tsx
@@ -2,19 +2,32 @@ import { InputField } from "components/input-field/input-field";
 import styles from "./loginStyle.module.scss";
 import { TitleHead } from "./title-head";
 import { ChangeEvent, useState } from "react";
+import { PrimaryButton } from "components/primary-button/primary-button";
 
-interface emailInpType {
+interface EmailInpType {
   value: string;
   isValid: boolean;
 }
+interface PasswordInpType {
+  value: string;
+  isValid: boolean;
+}
+
 export const Login = () => {
-  const [emailInp, setEmailInp] = useState<emailInpType>({
+  const [emailInp, setEmailInp] = useState<EmailInpType>({
+    value: "",
+    isValid: false,
+  });
+  const [passwordInp, setPasswordInp] = useState<PasswordInpType>({
     value: "",
     isValid: false,
   });
 
   function handleEmailInpChange(e: ChangeEvent<HTMLInputElement>) {
     setEmailInp({ value: e.target.value, isValid: true });
+  }
+  function handlePasswordInpChange(e: ChangeEvent<HTMLInputElement>) {
+    setPasswordInp({ value: e.target.value, isValid: true });
   }
   return (
     <div className={styles.container}>
@@ -33,8 +46,20 @@ export const Login = () => {
               type="text"
               error={"testingg..."}
             />
+            <InputField
+              error={null}
+              label="PASSWORD"
+              name="password"
+              onChange={handlePasswordInpChange}
+              placeholder="Password@123"
+              type="password"
+              value={passwordInp.value}
+            />
+            <div className={styles.buttonWrap}>
+              <PrimaryButton disabled={false} text="LOGIN" />
+            </div>
           </div>
-          <div>sso</div>
+          <div>SSO coming soon...</div>
         </div>
       </div>
     </div>
