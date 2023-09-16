bbed3d25c8e01dda81017ce16e1256fac75f0dfd
muhsin <muhsintt777@gmail.com>
2023-09-17 03:45:19 +0530

    feat : add email inp

--- src/screens/auth/login/login.tsx
+++ src/screens/auth/login/login.tsx
@@ -1,7 +1,21 @@
+import { InputField } from "components/input-field/input-field";
 import styles from "./loginStyle.module.scss";
 import { TitleHead } from "./title-head";
+import { ChangeEvent, useState } from "react";
 
+interface emailInpType {
+  value: string;
+  isValid: boolean;
+}
 export const Login = () => {
+  const [emailInp, setEmailInp] = useState<emailInpType>({
+    value: "",
+    isValid: false,
+  });
+
+  function handleEmailInpChange(e: ChangeEvent<HTMLInputElement>) {
+    setEmailInp({ value: e.target.value, isValid: true });
+  }
   return (
     <div className={styles.container}>
       <div className={styles.head}>
@@ -10,7 +24,15 @@ export const Login = () => {
       <div className={styles.main}>
         <div className={styles.loginBox}>
           <div>
-            <p>EMAIL</p>
+            <InputField
+              value={emailInp.value}
+              label="EMAIL"
+              name="email"
+              onChange={handleEmailInpChange}
+              placeholder="john@email.com"
+              type="text"
+              error={"testingg..."}
+            />
           </div>
           <div>sso</div>
         </div>
