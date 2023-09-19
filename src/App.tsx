093c80379da1ebc93fc16d0bbded7373abac59f9
muhsin <muhsintt777@gmail.com>
2023-09-19 23:06:17 +0530

    feat : add app loader structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PrimaryLayout } from "./layouts/primary-layout/primary-layout";
 import { AuthLayout } from "layouts/auth-layout/auth-layout";
@@ -6,17 +7,25 @@ import { Home } from "./screens/home/home";
 import { Login } from "screens/auth/login/login";
 
 export const App = () => {
+  const [showLoader, setShowLoader] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PrimaryLayout />}>
-          <Route path="home" index element={<Home />} />
-        </Route>
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<>sefsefe</>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      {showLoader ? (
+        <p>loadeinggg</p>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PrimaryLayout />}>
+              <Route path="home" index element={<Home />} />
+            </Route>
+            <Route path="/auth" element={<AuthLayout />}>
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<>sefsefe</>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      )}
+    </>
   );
 };
