fcb999c313ed7373989764c632feb4c5ed817474
Muhsin TT <muhsintt777@gmail.com>
2023-09-20 19:57:51 +0530

    feat : add get user api service

--- src/utils/api-service.ts
+++ src/utils/api-service.ts
@@ -1,7 +1,28 @@
 import { http } from "configs/http";
+import { GetCurrentUserSchema } from "./schema";
 
 export class ApiService {
   static async login(email: string, password: string) {
-    return http.post("auth/login", { email, password });
+    await http.post("auth/login", { email, password });
+  }
+
+  static async getCurrentUser() {
+    try {
+      const result = (await http.get("auth/user", { withCredentials: true }))
+        .data;
+
+      const user = GetCurrentUserSchema.safeParse(result);
+      if (user.success === false) {
+        throw { errorMessage: "Server error: Invalidated data received" };
+      } else {
+        return user;
+      }
+    } catch (err: any) {
+      if (err.errorMessage) {
+        throw { errorMessage: err.errorMessage };
+      } else {
+        throw { errorMessage: "Fetch auth user failed" };
+      }
+    }
   }
 }
