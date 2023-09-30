c4719080bf464f1e7efae87897fb3e6b8811d09e
muhsin <muhsintt777@gmail.com>
2023-10-01 00:58:58 +0530

    feat : add post content actions ui

--- src/pages/home/components/post/post.tsx
+++ src/pages/home/components/post/post.tsx
@@ -1,7 +1,12 @@
 import styles from "./postStyle.module.scss";
+
 import profilePicPNG from "assets/images/profile-pic.png";
+import test1 from "assets/images/test1.jpg";
 import { VerticalDotIcon } from "assets/icons-components/vertical-dot-icon";
 import { PrimaryIconButton } from "components/primary-icon-button/primary-icon-button";
+import { LikeIcon } from "assets/icons-components/like-icon";
+import { CommentIcon } from "assets/icons-components/comment-icon";
+import { ShareIcon } from "assets/icons-components/share-icon";
 
 export const Post = () => {
   return (
@@ -11,8 +16,8 @@ export const Post = () => {
           <img src={profilePicPNG} alt="img" />
         </div>
         <div>
-          <p>name</p>
-          <p>time ago</p>
+          <p>John Doe</p>
+          <p>5 mins ago</p>
         </div>
         <div className={styles.iconButton}>
           <PrimaryIconButton onClick={() => console.log("icon-button")}>
@@ -20,8 +25,17 @@ export const Post = () => {
           </PrimaryIconButton>
         </div>
       </div>
-      <div className={styles.content}>content</div>
-      <div className={styles.action}>actioon</div>
+      <div className={styles.content}>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae,
+        </p>
+        <img src={test1} alt="img" />
+      </div>
+      <div className={styles.actions}>
+        <LikeIcon />
+        <CommentIcon />
+        <ShareIcon />
+      </div>
     </article>
   );
 };
