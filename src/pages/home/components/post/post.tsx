33f779a73deea28eed29b4752203d13806aee30f
muhsin <muhsintt777@gmail.com>
2023-10-01 00:07:00 +0530

    feat : posts header style

--- src/pages/home/components/post/post.tsx
+++ src/pages/home/components/post/post.tsx
@@ -1,15 +1,24 @@
 import styles from "./postStyle.module.scss";
+import profilePicPNG from "assets/images/profile-pic.png";
+import { VerticalDotIcon } from "assets/icons-components/vertical-dot-icon";
+import { PrimaryIconButton } from "components/primary-icon-button/primary-icon-button";
 
 export const Post = () => {
   return (
     <article className={styles.container}>
       <div className={styles.head}>
-        <div>img</div>
+        <div className={styles.imageWrap}>
+          <img src={profilePicPNG} alt="img" />
+        </div>
         <div>
           <p>name</p>
           <p>time ago</p>
         </div>
-        <div>icon</div>
+        <div className={styles.iconButton}>
+          <PrimaryIconButton onClick={() => console.log("icon-button")}>
+            <VerticalDotIcon color="var(--clr-grey)" />
+          </PrimaryIconButton>
+        </div>
       </div>
       <div className={styles.content}>content</div>
       <div className={styles.action}>actioon</div>
