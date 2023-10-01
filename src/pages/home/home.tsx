563934911a3c6ae46a83fbe297f66fb877804d42
muhsin <muhsintt777@gmail.com>
2023-10-01 23:56:34 +0530

    feat : add home addPost

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,11 @@
 import styles from "./homeStyle.module.scss";
 import { Post } from "./components/post/post";
+import { AddPost } from "./components/add-post/add-post";
 
 export const Home = () => {
   return (
     <div className={styles.container}>
+      <AddPost />
       <div className={styles.postWrap}>
         <Post />
       </div>
