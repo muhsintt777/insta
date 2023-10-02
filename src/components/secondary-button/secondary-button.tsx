57629af5763f5fa611111a01a1a58b8e099f13c1
muhsin <muhsintt777@gmail.com>
2023-10-02 17:53:22 +0530

    chore : secondary button component

---
+++ src/components/secondary-button/secondary-button.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@mui/material";
+import { DotLoader } from "components/loaders/dot-loader";
+import { FC, ReactNode } from "react";
+
+interface SecondaryButtonProps {
+  children: ReactNode;
+  disabled?: boolean;
+  showLoader?: boolean;
+  fullWidth?: boolean;
+  hoverColor?: string;
+  color?: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+}
+
+export const SecondaryButton: FC<SecondaryButtonProps> = ({
+  children,
+  disabled = false,
+  showLoader = false,
+  fullWidth = false,
+  type = "button",
+  hoverColor = "none",
+  color = "var(--clr-grey)",
+}) => {
+  return (
+    <Button
+      type={type}
+      fullWidth={fullWidth}
+      disabled={disabled}
+      sx={{
+        textTransform: "none",
+        color: color,
+        "&:hover": {
+          backgroundColor: hoverColor,
+        },
+      }}
+    >
+      {showLoader ? <DotLoader /> : children}
+    </Button>
+  );
+};
