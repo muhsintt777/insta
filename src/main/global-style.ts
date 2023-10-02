697f39596e8587f33d7660ce3cc56853aeec4799
muhsin <muhsintt777@gmail.com>
2023-10-02 17:51:45 +0530

    chore : global style ts

---
+++ src/main/global-style.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  PRIMARY: "var(--clr-primary)",
+  PRIMARY_LIGHT: "var(--clr-primary-light)",
+  BLACK: "var(--clr-black)",
+  GREEN: "var(--clr-green)",
+  PURPLE: "var(--clr-purple)",
+  YELLOW: "var(--clr-yellow)",
+  GREY: "var(--clr-grey)",
+  LIGHTGREY: "var(--clr-lightGrey)",
+  BG_PRIMARY: "var(--clr-bg-primary)",
+};
+
+export const fonts = {
+  PRIMARY: "var(--fnt-primary)",
+  SECONDARY: "var(--fnt-secondary)",
+};
