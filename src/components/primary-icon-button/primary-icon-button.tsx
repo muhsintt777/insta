6b293bc671c738abbf2da5d259626ba9469345e7
muhsin <muhsintt777@gmail.com>
2023-10-02 15:53:57 +0530

    feat : add type to primary icon button

--- src/components/primary-icon-button/primary-icon-button.tsx
+++ src/components/primary-icon-button/primary-icon-button.tsx
@@ -3,12 +3,18 @@ import { IconButton } from "@mui/material";
 
 interface PrimaryIconButtonProps {
   children: ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
 }
 
 export const PrimaryIconButton: FC<PrimaryIconButtonProps> = ({
   children,
   onClick,
+  type = "button",
 }) => {
-  return <IconButton onClick={onClick}>{children}</IconButton>;
+  return (
+    <IconButton type={type} onClick={onClick}>
+      {children}
+    </IconButton>
+  );
 };
