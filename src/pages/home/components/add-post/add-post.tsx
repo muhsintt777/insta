519cd8c82892c651e3105902eedd36dfef5ff5f0
muhsin <muhsintt777@gmail.com>
2023-10-02 15:55:20 +0530

    feat : add send button to add post

--- src/pages/home/components/add-post/add-post.tsx
+++ src/pages/home/components/add-post/add-post.tsx
@@ -1,6 +1,9 @@
 import styles from "./add-postStyle.module.scss";
 import { FormEvent } from "react";
+
+import { SendIcon } from "assets/icons-components/send-icon";
 import { RoundedProfile } from "components/rounded-profile/rounded-profile";
+import { PrimaryIconButton } from "components/primary-icon-button/primary-icon-button";
 
 export const AddPost = () => {
   async function addPost(e: FormEvent<HTMLFormElement>) {
@@ -12,6 +15,9 @@ export const AddPost = () => {
       <RoundedProfile />
       <form onSubmit={addPost}>
         <input type="text" placeholder="What's on your mind ? <username>" />
+        <PrimaryIconButton type="submit">
+          <SendIcon size="12px" color="var(--clr-grey)" />
+        </PrimaryIconButton>
       </form>
     </div>
   );
