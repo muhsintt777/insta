5b76feabff1046061a58e74b8dbe388b67548f9b
Muhsin <muhsintt777@gmail.com>
2023-10-22 19:03:08 +0530

    feat : add file size constant , fix linter error

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,7 @@
 export const REGEX = {
-  email: /^[\w\.-]+[\+\w\.-]*@[\w\.-]+\.\w+$/,
+  email: /^[\w.-]+[+\w.-]*@[\w.-]+\.\w+$/,
   password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/,
   name: /^[A-Za-z]+$/,
 };
+
+export const FILE_SIZE = { postImage: 5 * 1024 * 1024 };
