476fdd3a2af13c3f9f0a50d271afdc5f1247e0e3
Muhsin <muhsintt777@gmail.com>
2024-12-22 13:09:38 +0530

    chore : add env config

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { ENV } from './env';
 
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URI,
+  baseURL: ENV.API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
