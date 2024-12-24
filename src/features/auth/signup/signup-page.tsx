d654a3194a2da88a1b497927408153d8badea2c9
Muhsin <muhsintt777@gmail.com>
2024-12-25 00:24:55 +0530

    feat : add create account form fields

--- src/features/auth/signup/signup-page.tsx
+++ src/features/auth/signup/signup-page.tsx
@@ -1,13 +1,15 @@
 import styles from './signup-page.module.scss';
+import { useCallback } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormField } from 'components/input-field/form-field';
+import { PrimaryButton } from 'components/primary-button/primary-button';
 import { signupFormSchema, SignupFormSchema } from './signup-schema';
 import { AuthHeader } from '../components/auth-header';
 
 export const SignupPage = () => {
   const {
-    // handleSubmit,
+    handleSubmit,
     control,
     formState: { errors },
   } = useForm<SignupFormSchema>({
@@ -22,29 +24,115 @@ export const SignupPage = () => {
     },
   });
 
+  const onSubmit = useCallback((data: SignupFormSchema) => {
+    console.log(data);
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.head}>
         <AuthHeader title="SIGNUP" />
       </div>
       <div className={styles.main}>
-        <Controller
-          name="fullName"
-          control={control}
-          render={({ field }) => (
-            <FormField
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.rowFields}>
+            <Controller
               name="fullName"
-              label="Full name"
-              error={errors.fullName?.message || null}
-              placeholder="John Doe"
-              controls={{
-                type: 'TEXT',
-                onchange: field.onChange,
-                value: field.value,
-              }}
+              control={control}
+              render={({ field }) => (
+                <FormField
+                  containerStyle={{ width: '300px' }}
+                  name="fullName"
+                  label="Full name"
+                  error={errors.fullName?.message || null}
+                  placeholder="John Doe"
+                  controls={{
+                    type: 'TEXT',
+                    onchange: field.onChange,
+                    value: field.value,
+                  }}
+                />
+              )}
             />
-          )}
-        />
+            <Controller
+              name="userName"
+              control={control}
+              render={({ field }) => (
+                <FormField
+                  containerStyle={{ width: '300px' }}
+                  name="userName"
+                  label="Username"
+                  error={errors.userName?.message || null}
+                  placeholder="john_doe_123"
+                  controls={{
+                    type: 'TEXT',
+                    onchange: field.onChange,
+                    value: field.value,
+                  }}
+                />
+              )}
+            />
+          </div>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <FormField
+                containerStyle={{ width: '616px' }}
+                name="email"
+                label="Email"
+                error={errors.email?.message || null}
+                placeholder="johndoe@gmail.com"
+                controls={{
+                  type: 'TEXT',
+                  onchange: field.onChange,
+                  value: field.value,
+                }}
+              />
+            )}
+          />
+
+          <div className={styles.rowFields}>
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <FormField
+                  containerStyle={{ width: '300px' }}
+                  name={field.name}
+                  label="Password"
+                  error={errors.password?.message || null}
+                  placeholder="********"
+                  controls={{
+                    type: 'PASSWORD',
+                    onchange: field.onChange,
+                    value: field.value,
+                  }}
+                />
+              )}
+            />
+
+            <Controller
+              name="confirmPassword"
+              control={control}
+              render={({ field }) => (
+                <FormField
+                  containerStyle={{ width: '300px' }}
+                  name={field.name}
+                  label="Confirm Password"
+                  error={errors.confirmPassword?.message || null}
+                  placeholder="********"
+                  controls={{
+                    type: 'PASSWORD',
+                    onchange: field.onChange,
+                    value: field.value,
+                  }}
+                />
+              )}
+            />
+          </div>
+          <PrimaryButton type="submit" text="CREATE ACCOUNT" />
+        </form>
       </div>
     </div>
   );
