522ed4f9be91ff6afade4716a195152656313df9
Muhsin <muhsintt777@gmail.com>
2024-12-25 11:34:13 +0530

    feat : create error toast handler

--- src/utils/error-handlers.ts
+++ src/utils/error-handlers.ts
@@ -1,6 +1,7 @@
 import { AxiosError } from 'axios';
 import { ERROR_TYPE } from 'configs/constants';
 import { CustomError } from './custom-error';
+import { ToastSeverity } from 'features/toast/toast-types';
 
 export const apiErrorHandler = (error: any) => {
   if (error instanceof AxiosError) {
@@ -14,5 +15,18 @@ export const apiErrorHandler = (error: any) => {
     throw error;
   } else {
     throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Unknown error');
+  }
+};
+
+export const toastErrorHandler = (
+  error: any,
+  showToast: (severity: ToastSeverity, message: string) => void,
+) => {
+  if (error instanceof CustomError) {
+    showToast('error', error.message);
+  } else if (error instanceof Error) {
+    showToast('error', error.message);
+  } else {
+    showToast('error', 'Something went wrong');
   }
 };
