b36169797b00f40cddd8d4471985ea2b1543b6f6
Muhsin <muhsintt777@gmail.com>
2024-12-26 00:48:47 +0530

    feat  : create user types

---
+++ src/features/user/user-types.ts
@@ -0,0 +1,37 @@
+export interface User {
+  _id: string;
+  email: string;
+  username: string;
+  fullName: string;
+  profileImage: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+}
+
+interface UserSuccessSlice {
+  status: 'SUCCESS';
+  details: User;
+}
+
+interface UserFailedSlice {
+  status: 'FAILED';
+  details: null;
+  error: string;
+}
+
+interface UserLoadingSlice {
+  status: 'LOADING';
+  details: null;
+}
+
+interface UserLoggedOutSlice {
+  status: 'LOGGED_OUT';
+  details: null;
+}
+
+export type UserSlice =
+  | UserSuccessSlice
+  | UserFailedSlice
+  | UserLoadingSlice
+  | UserLoggedOutSlice;
