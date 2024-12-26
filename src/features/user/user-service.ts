54adbf0b469f82d1460dbc7e775e61c79df892e2
Muhsin <muhsintt777@gmail.com>
2024-12-27 00:19:50 +0530

    feat : create user service, async handler

---
+++ src/features/user/user-service.ts
@@ -0,0 +1,20 @@
+import { http } from 'configs/http';
+import { ERROR_TYPE, HTTP_STATUS_CODES } from 'configs/constants';
+import { CustomError } from 'utils/custom-error';
+import { asyncHandler } from 'utils/common';
+import { User } from './user-types';
+
+export const userService = {
+  fetchCurrentUser: async () => {
+    return asyncHandler(async () => {
+      const result = await http.get('users/me');
+      if (result.status !== HTTP_STATUS_CODES.OK) {
+        throw new CustomError(
+          ERROR_TYPE.UNKNOWN_API_ERROR,
+          'Fetch user failed',
+        );
+      }
+      return result.data?.data as User;
+    })();
+  },
+};
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,28 @@
+import { AxiosError } from 'axios';
+import { ERROR_TYPE } from 'configs/constants';
+import { CustomError } from './custom-error';
+
 export const trimAllWhitespace = (text: string): string => {
   return text.replace(/\s+/g, '');
+};
+
+export const asyncHandler = <T>(fn: () => Promise<T>) => {
+  return async () => {
+    try {
+      return fn();
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      } else if (error instanceof AxiosError) {
+        throw new CustomError(
+          error.response?.data.errorType,
+          error.response?.data.errorMessage,
+        );
+      } else if (error instanceof Error) {
+        throw new CustomError(ERROR_TYPE.NODE_ERROR, error.message);
+      } else {
+        throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Unknown error');
+      }
+    }
+  };
 };
