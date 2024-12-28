edeb0b17b65e515727a78eedfd908d413aaddca0
Muhsin <muhsintt777@gmail.com>
2024-12-28 23:36:13 +0530

    chore : code refactor

--- src/utils/error-handlers.ts
+++ src/utils/error-handlers.ts
@@ -1,22 +1,5 @@
-import { AxiosError } from 'axios';
-import { ERROR_TYPE } from 'configs/constants';
 import { CustomError } from './custom-error';
 import { ToastSeverity } from 'features/toast/toast-types';
-
-export const rethrowCustomError = (error: any) => {
-  if (error instanceof AxiosError) {
-    throw new CustomError(
-      error.response?.data.errorType,
-      error.response?.data.errorMessage,
-    );
-  } else if (error instanceof Error) {
-    throw new CustomError(ERROR_TYPE.NODE_ERROR, error.message);
-  } else if (error instanceof CustomError) {
-    throw error;
-  } else {
-    throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Unknown error');
-  }
-};
 
 export const toastErrorHandler = (
   error: any,
