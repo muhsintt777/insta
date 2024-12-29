fbf4aa8051a167ae2faed6e560e1fa10b9e4c1df
Muhsin <muhsintt777@gmail.com>
2024-12-29 11:10:20 +0530

    feat : add header profile section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,18 @@
-import styles from './style.module.css';
+import { RoundedProfile } from 'components/rounded-profile/rounded-profile';
+import styles from './header.module.scss';
 
 export const Header = () => {
-  return <header className={styles.container}>Header</header>;
+  return (
+    <header className={styles.container}>
+      <div className="app-container">
+        <div className={styles.contentWrap}>
+          <div>logo</div>
+          <div className={styles.profile}>
+            <RoundedProfile />
+            <p>John Doe</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
