c795a4c12983b6ea7c521f7d65ac8de0d2dcc697
Muhsin <muhsintt777@gmail.com>
2024-12-30 18:57:06 +0530

    feat :  refresh auth token handling

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -17,19 +17,22 @@ http.interceptors.response.use(
     return res;
   },
   async (err) => {
+    const statusCode = err.response?.data?.statusCode;
+    const errorType = err.response?.data?.errorType;
+
     if (
-      err.response?.data.statusCode === HTTP_STATUS_CODES.UNAUTHORIZED &&
-      err.response?.data.errorType === ERROR_TYPE.AUTH_TOKEN_EXPIRED
+      statusCode === HTTP_STATUS_CODES.UNAUTHORIZED &&
+      errorType === ERROR_TYPE.AUTH_TOKEN_EXPIRED
     ) {
-      try {
-        await http.post('auth/refresh');
-        return http.request(err.config);
-      } catch (refreshError) {
-        store.dispatch(logout());
-        throw refreshError;
-      }
-    } else {
-      throw err;
+      await http.post('auth/refresh');
+      return http.request(err.config);
+    } else if (
+      statusCode === HTTP_STATUS_CODES.UNAUTHORIZED &&
+      errorType !== ERROR_TYPE.AUTH_TOKEN_EXPIRED
+    ) {
+      store.dispatch(logout());
     }
+
+    throw err;
   },
 );
