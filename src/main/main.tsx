8e84ddec41cc029a1a488b8b4f685df982c01a6e
Muhsin <muhsintt777@gmail.com>
2024-12-30 19:09:14 +0530

    fix : import path

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'configs/store.ts';
-import { App } from './App';
+import { App } from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
