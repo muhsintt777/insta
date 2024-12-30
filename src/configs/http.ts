d24506329f6d71810c9a76c222d24828ad94be14
Muhsin <muhsintt777@gmail.com>
2024-12-30 17:18:23 +0530

    feat : add token refresh

--- src/configs/http.ts
+++ src/configs/http.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { logout } from 'features/user/userSlice';
+import { ERROR_TYPE, HTTP_STATUS_CODES } from './constants';
+import { store } from './store';
 import { ENV } from './env';
 
 export const http = axios.create({
@@ -8,3 +11,25 @@ export const http = axios.create({
   },
   withCredentials: true,
 });
+
+http.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    if (
+      err.response?.data.statusCode === HTTP_STATUS_CODES.UNAUTHORIZED &&
+      err.response?.data.errorType === ERROR_TYPE.AUTH_TOKEN_EXPIRED
+    ) {
+      try {
+        await http.post('auth/refresh');
+        return http.request(err.config);
+      } catch (refreshError) {
+        store.dispatch(logout());
+        throw refreshError;
+      }
+    } else {
+      throw err;
+    }
+  },
+);
