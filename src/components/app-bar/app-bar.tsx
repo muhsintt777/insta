c382419591d45650c4746324a93a22f2ca6bfcd9
Muhsin <muhsintt777@gmail.com>
2024-12-31 20:17:34 +0530

    feat : create app bar

---
+++ src/components/app-bar/app-bar.tsx
@@ -0,0 +1,22 @@
+import styles from './app-bar.module.scss';
+import { FC } from 'react';
+import { BackIcon } from 'assets/icons-components/back-icon';
+import { PrimaryIconButton } from 'components/primary-icon-button/primary-icon-button';
+
+interface AppBarProps {
+  title: string;
+  onBackClick: () => void;
+}
+
+export const AppBar: FC<AppBarProps> = ({ title, onBackClick }) => {
+  return (
+    <div className={styles.container}>
+      <PrimaryIconButton onClick={onBackClick}>
+        <div className={styles.iconWrap}>
+          <BackIcon size="18px" />
+        </div>
+      </PrimaryIconButton>
+      <p>{title}</p>
+    </div>
+  );
+};
