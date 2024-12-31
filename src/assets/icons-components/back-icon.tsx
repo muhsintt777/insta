e7df3defeb053f6a62c2b497bec64e3f014fa20d
Muhsin <muhsintt777@gmail.com>
2024-12-31 20:15:15 +0530

    feat : create back icon compoent

---
+++ src/assets/icons-components/back-icon.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { colors } from 'main/global-style';
+
+interface BackIconProps {
+  color?: string;
+  size?: string;
+}
+
+export const BackIcon: FC<BackIconProps> = ({
+  color = colors.BLACK,
+  size = '16px',
+}) => {
+  return (
+    <svg
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      fill={color}
+      height={size}
+      width={size}
+    >
+      <path d="M11.67 3.87 9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"></path>
+    </svg>
+  );
+};
