091f25d437a20373b1cfa8216eb419ccdd49ae4a
Muhsin <muhsintt777@gmail.com>
2024-12-31 20:17:46 +0530

    feat : add app bar

--- src/features/user/profile-page.tsx
+++ src/features/user/profile-page.tsx
@@ -1,5 +1,13 @@
 import styles from './profile-page.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { AppBar } from 'components/app-bar/app-bar';
 
 export const ProfilePage = () => {
-  return <div className={styles.container}>profile</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <AppBar title="PROFILE" onBackClick={() => navigate(-1)} />
+    </div>
+  );
 };
