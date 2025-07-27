65adda70981242f9cba15171f3bbac89782040a5
Muhsin <muhsintt777@gmail.com>
2025-07-27 17:50:28 +0530

    chore: code refactor

--- src/features/auth/auth-service.ts
+++ src/features/auth/auth-service.ts
@@ -1,6 +1,5 @@
 import { ERROR_TYPE, HTTP_STATUS_CODES } from 'configs/constants';
 import { http } from 'configs/http';
-import { asyncHandler } from 'utils/common';
 import { CustomError } from 'utils/custom-error';
 
 interface CreateUserParams {
@@ -16,33 +15,27 @@ type LoginParams =
 
 export class AuthService {
   static async createUser(params: CreateUserParams) {
-    return asyncHandler(async () => {
-      const { status } = await http.post('users', params);
-      if (status !== HTTP_STATUS_CODES.CREATED) {
-        throw new CustomError(
-          ERROR_TYPE.UNKNOWN_API_ERROR,
-          'Failed to create user',
-        );
-      }
-    });
+    const { status } = await http.post('users', params);
+    if (status !== HTTP_STATUS_CODES.CREATED) {
+      throw new CustomError(
+        ERROR_TYPE.UNKNOWN_API_ERROR,
+        'Failed to create user',
+      );
+    }
   }
 
   static async login(params: LoginParams) {
-    return asyncHandler(async () => {
-      const response = await http.post('auth/login', params);
-      if (response.status !== HTTP_STATUS_CODES.OK) {
-        throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Failed to login');
-      }
-    });
+    const response = await http.post('auth/login', params);
+    if (response.status !== HTTP_STATUS_CODES.OK) {
+      throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Failed to login');
+    }
   }
 
   static async signout() {
-    return asyncHandler(async () => {
-      const result = (await http.post('auth/logout')).data;
-      if (result.statusCode !== HTTP_STATUS_CODES.OK) {
-        throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Failed to logout');
-      }
-    });
+    const result = (await http.post('auth/logout')).data;
+    if (result.statusCode !== HTTP_STATUS_CODES.OK) {
+      throw new CustomError(ERROR_TYPE.UNKNOWN_API_ERROR, 'Failed to logout');
+    }
   }
 
   static async refreshAuth() {
