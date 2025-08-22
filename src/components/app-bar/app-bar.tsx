d1ba2a524b33e89e33c7e2a2b30d2c440209590f
Muhsin <muhsintt777@gmail.com>
2025-08-22 09:24:59 +0530

    feat: ad custom styles

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,16 +1,21 @@
 import styles from './app-bar.module.scss';
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 import { BackIcon } from 'assets/icons-components/back-icon';
 import { PrimaryIconButton } from 'components/buttons/primary-icon-button';
 
 interface AppBarProps {
   title: string;
   onBackClick: () => void;
+  customStyles?: CSSProperties;
 }
 
-export const AppBar: FC<AppBarProps> = ({ title, onBackClick }) => {
+export const AppBar: FC<AppBarProps> = ({
+  title,
+  onBackClick,
+  customStyles,
+}) => {
   return (
-    <div className={styles.container}>
+    <div style={customStyles} className={styles.container}>
       <PrimaryIconButton onClick={onBackClick}>
         <div className={styles.iconWrap}>
           <BackIcon size="18px" />
