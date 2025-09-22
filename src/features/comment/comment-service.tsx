1cbacb32be793f04b3e43544c894a3dd2581a6cc
Muhsin <muhsintt777@gmail.com>
2025-09-22 15:42:44 +0530

    feat: create comment service

---
+++ src/features/comment/comment-service.tsx
@@ -0,0 +1,21 @@
+import { http } from 'configs/http';
+
+export class CommentService {
+  static async listPostComments(postId: string) {
+    const result = await http.get(`/comments/post/${postId}`);
+    if (result.status === 204) return [];
+    if (result.status !== 200) throw new Error('Failed to fetch comments');
+    return result.data;
+  }
+
+  static async createComment(postId: string, content: string) {
+    const result = await http.post('/comments', { content, postId });
+    if (result.status !== 201) throw new Error('Failed to create comment');
+    return result.data.data;
+  }
+
+  static async deleteComment(commentId: string) {
+    const result = await http.delete(`/comments/${commentId}`);
+    if (result.status !== 200) throw new Error('Failed to delete comment');
+  }
+}
