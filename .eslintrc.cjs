475d745e4ac760aab11849cb2fc41f9a1c903746
Muhsin <muhsintt777@gmail.com>
2025-09-26 17:43:29 +0530

    fix: eslint ts build conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 };
