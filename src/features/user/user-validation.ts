ba744d09fd983b16591267cebf411d5aa420ef56
Muhsin <muhsintt777@gmail.com>
2025-09-30 13:52:56 +0530

    fix: bio validation

--- src/features/user/user-validation.ts
+++ src/features/user/user-validation.ts
@@ -3,13 +3,9 @@ import { z } from 'zod';
 export const userEditSchema = z.object({
   fullname: z
     .string()
-    .min(1, 'Fullname is too small')
+    .min(2, 'Fullname is too small')
     .max(100, 'Fullname is too long'),
-  bio: z
-    .string()
-    .min(1, 'Bio is too small')
-    .max(200, 'Bio is too long')
-    .nullable(),
+  bio: z.string().max(200, 'Bio is too long').nullable(),
 });
 
 export type UserEditSchema = z.infer<typeof userEditSchema>;
