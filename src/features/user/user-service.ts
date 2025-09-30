35e541d88b003c59d01d5019db620d8cd095fb80
Muhsin <muhsintt777@gmail.com>
2025-09-30 13:52:05 +0530

    feat: add edit user function

--- src/features/user/user-service.ts
+++ src/features/user/user-service.ts
@@ -2,6 +2,10 @@ import { http } from 'configs/http';
 import { HTTP_STATUS_CODES } from 'configs/constants';
 import { User } from './user';
 
+interface EditUserProfileParams {
+  fullName?: string;
+  bio?: string;
+}
 export class UserService {
   static async fetchCurrentUser(): Promise<User> {
     const res = await http.get('/users/currentuser');
@@ -9,5 +13,14 @@ export class UserService {
       throw new Error('Failed to fetch user details');
     }
     return res.data.data as User;
+  }
+
+  static async editUserProfile(params: EditUserProfileParams) {
+    console.log('params', params);
+
+    // const res = await http.put('users/currentuser', params);
+    // if (res.status !== HTTP_STATUS_CODES.OK) {
+    //   throw new Error('Failed to update user profile');
+    // }
   }
 }
