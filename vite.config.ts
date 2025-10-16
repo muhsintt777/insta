eb7df4a52056056a6ddf673013f6cbb555648492
Muhsin <muhsintt777@gmail.com>
2025-10-16 13:08:32 +0530

    chore: remove vite static copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: 'public/_redirects', dest: '.' }],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       assets: '/src/assets',
@@ -23,3 +17,8 @@ export default defineConfig({
     },
   },
 });
+
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
+// viteStaticCopy({
+//       targets: [{ src: 'public/_redirects', dest: '.' }],
+//     }),
