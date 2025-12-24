5959643850432e360812e0f7cbdc51c63b735efb
Muhsin <muhsintt777@gmail.com>
2025-12-25 00:07:33 +0530

    feat: create theme slice

---
+++ src/features/theme/theme-slice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { Rootstate } from 'configs/store';
+
+const STORAGE_KEY = 'app:theme';
+
+type Theme = 'light' | 'dark';
+interface ThemeState {
+  theme: Theme;
+}
+
+const initialState: ThemeState = {
+  theme: 'light',
+};
+
+export const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      const newTheme = state.theme === 'light' ? 'dark' : 'light';
+      localStorage.setItem(STORAGE_KEY, newTheme);
+      document.documentElement.setAttribute('data-theme', newTheme);
+      state.theme = newTheme;
+    },
+  },
+});
+
+export const themeActions = themeSlice.actions;
+
+export const selectTheme = (state: Rootstate) => state.theme.theme;
+
+export const themeReducer = themeSlice.reducer;
