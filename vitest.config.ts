bdf9f9bd7590d5ef465e6b6003c0e1e2a64a43e2
Muhsin <muhsintt777@gmail.com>
2026-01-05 00:13:39 +0530

    fix: test import issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -9,6 +10,21 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
+    },
+    deps: {
+      inline: ['get-intrinsic', 'es-abstract', 'es-define-property'],
+    },
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      configs: path.resolve(__dirname, 'src/configs'),
+      features: path.resolve(__dirname, 'src/features'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
+      main: path.resolve(__dirname, 'src/main'),
+      utils: path.resolve(__dirname, 'src/utils'),
     },
   },
 });
