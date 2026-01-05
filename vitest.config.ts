908a025817adac60ae023cc58bb68776b71c2182
Muhsin <muhsintt777@gmail.com>
2026-01-06 00:16:40 +0530

    chore: vite dom updated

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     globals: true,
     setupFiles: 'src/setupTests.ts',
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
